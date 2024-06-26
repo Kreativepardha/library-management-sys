074e4ac218eab4a5f941f914fe6b2207487dc217
Kreativepardha <saradhipardha12@gmail.com>
2024-06-26 18:04:04 +0530

    added routes

--- lms-fe/src/App.tsx
+++ lms-fe/src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import { AddBook } from './pages/AddBook'
+import { Login } from './pages/Login'
+import { Signup } from './pages/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div>
-        {/* <BookForm /> */}
-        <AddBook />
-    </div>
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />}  />
+          <Route path='/signup' element={<Signup />}  />
+          <Route path='/books' element={<AddBook />}  />
+        </Routes>
+        </BrowserRouter>
   )
 }
 
