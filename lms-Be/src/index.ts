5633717fc3b60682254758a9aef41c0505c6279e
Kreativepardha <saradhipardha12@gmail.com>
2024-07-22 20:53:10 +0530

    ?

--- lms-Be/src/index.ts
+++ lms-Be/src/index.ts
@@ -16,7 +16,7 @@ const dbUrl = process.env.MONGO_URL as string;
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173', 
+   origin: process.env.CORS_ORIGIN || '*'
     credentials: true
 }));
 
