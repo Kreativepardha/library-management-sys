6f18811c9244bc3d180533d42feb291f5c3d8359
Kreativepardha <116539060+Kreativepardha@users.noreply.github.com>
2024-08-15 18:07:14 +0530

    Update config.ts

--- lms-fe/src/config.ts
+++ lms-fe/src/config.ts
@@ -1,5 +1,5 @@
 
 
-export const BACKEND_URL = "https://library-management-sys-g14jzj2yy-pardhas-projects.vercel.app"
+export const BACKEND_URL = "https://library-management-sys-ksoxo1sid-pardhas-projects.vercel.app"
 // export const BACKEND_URL = 'http://localhost:3000'
 
